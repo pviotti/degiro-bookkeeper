d1120b73aea1efa4ababffa09a0f0bd33ed0f8fe
Paolo Viotti <skypixel@gmail.com>
2021-05-26 09:14:00 +0100

    chore: move main function into Main.fs
    Signed-off-by: Paolo Viotti <skypixel@gmail.com>

---
+++ src/Main.fs
@@ -0,0 +1,70 @@
+open System
+open System.IO
+
+open StockWatch.Degiro
+open StockWatch.Degiro.DegiroAccount
+
+[<EntryPoint>]
+let main argv =
+
+    if argv.Length < 3 then
+        eprintfn "Error: missing parameter"
+        eprintfn "Usage: stock-watcher <path/to/statement.csv> <year> [<1,2>]"
+        Environment.Exit 1
+
+    //let [<Literal>] csvFile = __SOURCE_DIRECTORY__ + "/account.csv"
+    // let csvFile = __SOURCE_DIRECTORY__ + string (Path.DirectorySeparatorChar) + argv.[2]
+    let originalCsvContent = File.ReadAllText argv.[0]
+    let cleanCsv, malformed = cleanCsv originalCsvContent
+
+    if malformed then
+        File.WriteAllText((argv.[0] + "-clean.csv"), cleanCsv)
+
+    let account = Account.Parse(cleanCsv)
+    let year = int argv.[1]
+
+    let period =
+        if argv.Length <= 2 then
+            Period.All
+        else
+            enum<Period> (int argv.[2])
+
+    let timer = new Diagnostics.Stopwatch()
+    timer.Start()
+    let txnsGrouped = getAllTxnRowsGrouped account
+    let txns = Seq.map buildTxn txnsGrouped
+    let sellsInPeriod = getSellTxnsInPeriod txns year period
+
+    if Seq.isEmpty sellsInPeriod then
+        printfn $"No sells recorded in %d{year}, period %A{period}."
+        Environment.Exit 0
+
+    printfn "%-10s %-40s %7s %8s\n%s" "Date" "Product" "P/L (€)" "P/L %" (String.replicate 68 "-")
+
+    let printEarning (e: Earning) =
+        printfn "%s %-40s %7.2f %7.1f%%" (e.Date.ToString("yyyy-MM-dd")) e.Product e.Value e.Percent
+
+    let periodEarnings = getSellsEarnings sellsInPeriod txns
+
+    Seq.toList periodEarnings
+    |> List.iter printEarning
+
+    let periodTotalEarnings =
+        periodEarnings |> Seq.sumBy (fun x -> x.Value)
+
+    let periodAvgPercEarnings =
+        periodEarnings
+        |> Seq.averageBy (fun x -> x.Percent)
+
+    printfn $"\nTot. P/L (€): %.2f{periodTotalEarnings}"
+    printfn $"Avg %% P/L: %.2f{periodAvgPercEarnings}%%"
+
+    let yearTotFees = getTotalYearFees account year
+    printfn $"\nTot. DeGiro fees in %d{year} (€): %.2f{yearTotFees}"
+
+    let totDeposits = getTotalDeposits account
+    let totYearDeposits = getTotalYearDeposits account year
+    printfn $"\nTot. deposits (€): %.2f{totDeposits}"
+    printfn $"Tot. deposits in %d{year} (€): %.2f{totYearDeposits}"
+    printfn $"\nElapsed time: {timer.ElapsedMilliseconds} ms"
+    0
